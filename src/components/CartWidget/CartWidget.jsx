8d52ffaa871adb01de0b315e688ed6a48a64ac79
Gabriel Ojeda Orias <gojedaor@ZCO-C02F15QJQ05D.local>
2021-07-24 12:18:07 -0400

    se agrego funcionalidad al componente itemCount

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './CartWidget.scss';
+import './CartWidget.scss';
 
 const CartWidget = () => (
   <div>
--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,17 +1,32 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './ItemCount.scss';
+import React, { useState } from 'react';
+import './ItemCount.scss';
 
-const ItemCount = () => (
-  <div>
-    <button>-</button>
-    <input type="text" name="count" id="count" className="inputCount" placeholder="1"/>
-    <button>+</button>
-  </div>
-);
+const ItemCount = ({ stock, initial}) => {
 
-ItemCount.propTypes = {};
+  const [count, setCount] = useState(initial);
+  const [stockStatus, setStockStatus] = useState(stock);
 
-ItemCount.defaultProps = {};
+  const onAdd = () => {
+    setStockStatus (stockStatus === 0 ? 0 :stockStatus - 1);
+    console.log(stockStatus);
+    stockStatus <= 0 ? setCount(5) : setCount(count === 5 ? 5 : count + 1); 
+    console.log(count);
+  };
 
+  const onRemove = () => {
+    setStockStatus (stockStatus === 5 ? 5 :stockStatus + 1);
+    console.log(stockStatus);
+    stockStatus >= 5 ? setCount(1) : setCount(count === 1 ? 1 : count - 1);
+    console.log(count);
+  };
+
+  return (
+    <div>
+      <button onClick={onRemove} className="buttonCount">-</button>
+     <input type="number" className="inputCount" value={count} onChange={e => onAdd(e.target.value)} />
+      <button className="buttonCount" onClick={onAdd}>+</button>
+      <p>{initial}</p>
+    </div>
+  )
+};
 export default ItemCount;
