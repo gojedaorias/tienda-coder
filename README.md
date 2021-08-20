a09242e32f99c4d55b8466ddb34274ced5fc7c29
Gabriel Ojeda Orias <gojedaor@ZCO-C02F15QJQ05D.local>
2021-08-20 12:52:33 -0400

    app terminada

--- README.md
+++ README.md
@@ -7,6 +7,9 @@
 ### UseForm
 [react hook form](https://react-hook-form.com/)
 
+Custom Hook usado para el manejo de formularios, facilita la creacion de formularios y fue usado tambien,
+con el proposito de aprender a usar custom hook externos.
+
 `npm install react-hook-form `   
 
 
@@ -14,6 +17,8 @@
 [Node Sass](https://sass-lang.com/)
 
 `npm install node-sass`
+
+Traspilador de Css usado para agilizar la escritura de CSS.
 
 ## Listado de componentes
 
--- src/components/CartForm/CartForm.jsx
+++ src/components/CartForm/CartForm.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState,useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 import { CartContext } from '../../contexts/CartContext'
@@ -10,17 +10,32 @@ import './CartForm.scss'
 
 
 const CartForm = () => {
-  const { register, formState: { errors } } = useForm();
+  const { register } = useForm();
   const { cartList, setCartList, cartPrice } = useContext(CartContext);
   const [buyer, setBuyer] = useState({});
   const [order, setOrder] = useState({ buyer, item: cartList, date: firebase.firestore.Timestamp.fromDate(new Date()), total: cartPrice });
   const [orderCode, setOrderCode] = useState('');
   const [clickSend, setClickSend] = useState(false);
+  const [validate, setValidate] = useState(false);
+
+  
+  useEffect(() => {
+    if (buyer.email === buyer.emailVerification && Boolean(buyer.emailVerification) === true){
+      setValidate(true);
+    }else{
+      setValidate(false);
+    }
+  }, [buyer.email, buyer.emailVerification]);
+
+  
 
   const handlerChange = (e) => {
-    setBuyer({ ...buyer, [e.target.name]: e.target.value });
+    setBuyer({ ...buyer, [e.target.name]: e.target.value});
     setOrder({ buyer, item: cartList });
   };
+
+
+
   const handlerSubmit = (e) => {
     e.preventDefault();
     setOrder({ buyer, item: cartList, date: firebase.firestore.Timestamp.fromDate(new Date()) });
@@ -30,7 +45,7 @@ const CartForm = () => {
         setOrderCode(docRef.id)
       })
     setClickSend(true);
-    console.log(order);
+    
   }
 
   const returnHome = () => {
@@ -38,7 +53,7 @@ const CartForm = () => {
     setCartList([]);
     setClickSend(false);
   }
-
+  
 
   return (
 
@@ -53,12 +68,12 @@ const CartForm = () => {
             <input placeholder="Ej: Peralta" className="cartform__input" {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
             <label htmlFor="lastName" className="cartform__label">Telefono</label>
             <input placeholder="Ej: 912345678" className="cartform__input" type="number" {...register("Telefono", { required: true, minLength: 9, maxLength: 9 })} />
-            <p>{errors.telefono?.message}</p>
             <label htmlFor="email" className="cartform__label">E-mail</label>
-            <input placeholder="Ej: fulanito@peralta.com" className="cartform__input" type="email" {...register("email", { required: true, maxLength: 100 })} />
+            <input placeholder="Ej: fulanito@peralta.com" className={`cartform__input ${validate  === false && Boolean(buyer.emailVerification) === true ? 'cartform__input--error':''}`} type="email" {...register("email", {pattern: /^\S+@\S+$/i, required: true, maxLength: 100 })} />
             <label htmlFor="email Verification" className="cartform__label">Verifica E-mail</label>
-            <input placeholder="Ej: fulanito@peralta.com" className="cartform__input" type="email" {...register("email Verification", { required: true, maxLength: 100 })} />
-            <input className="cartform__submit" type="submit" />
+            <input placeholder="Ej: fulanito@peralta.com" className={`cartform__input ${validate  === false && Boolean(buyer.emailVerification) === true ? 'cartform__input--error':''}`} type="email" {...register("emailVerification", {pattern: /^\S+@\S+$/i, required: true, maxLength: 100 })} />
+            <p className="cartform__emailerror">{validate  === false && Boolean(buyer.emailVerification) === true ? 'Tu correo no corresponde con el anterior': ''}</p>
+            <input className="cartform__submit" type="submit" disabled={ validate === true ? false:true} />
           </form>
         </div>
         ) :
@@ -68,7 +83,7 @@ const CartForm = () => {
           <div className="form">
             <h1 className="formtitle">Este el código de tu Orden</h1>
             <h2 className="formOrder">{orderCode}</h2>
-            <Link onClick={returnHome} to="/" className="form__link">Volver al Inicio</Link>
+            <Link onClick={returnHome} to="/" className="form__link" >Volver al Inicio</Link>
           </div>)}
     </>
 
